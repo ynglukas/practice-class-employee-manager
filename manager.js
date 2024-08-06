77369c606ca5c71c3b82c271ae0df842c68a7149
lanre <islukmon@gmail.com>
2024-08-06 14:49:30 +0100

    calculate Manager bonus

--- manager.js
+++ manager.js
@@ -9,6 +9,23 @@ class Manager extends Employee {
     addEmployee(employee) {
         this.employees.push(employee)
     }
+
+    _totalSubSalary() {
+        let sum = 0;
+        for(const employee of this.employees) {
+            if(employee instanceof Manager) {
+                sum += employee.salary + employee._totalSubSalary();
+            }else {
+                sum += employee.salary
+            }
+        }
+
+        return sum;
+    }
+
+    calculateBonus(multiplier) {
+        return (this.salary + this._totalSubSalary()) * multiplier;
+    }
 }
 
 
